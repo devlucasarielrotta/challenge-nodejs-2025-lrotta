3c792861ac5094a6c5ff0920c7aea3ad0cc7fad5
devlucasarielrotta <lucas.ariel.rotta@gmail.com>
2025-10-14 23:28:22 -0300

    added docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
 import { SeedModule } from './seed/seed.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [OrdersModule, SeedModule],
+  imports: [
+      ConfigModule.forRoot(),
+      OrdersModule, 
+      SeedModule, 
+      SequelizeModule.forRoot({
+        dialect:'postgres',
+        host: process.env.DB_HOST, // localhost
+        port: +process.env.DB_PORT!,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        autoLoadModels: true,
+        models: [],
+      })
+  ],
   controllers: [],
   providers: [],
 })
