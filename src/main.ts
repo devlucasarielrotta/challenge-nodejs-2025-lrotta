eecb196be7d8815e030ca6c458935accce0c39b6
devlucasarielrotta <lucas.ariel.rotta@gmail.com>
2025-10-15 19:51:09 -0300

    fix main

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,
+    {
+      logger:new ConsoleLogger({
+        colors:true,
+        prefix:'Orders -',
+        json:true
+      })
+    }
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true
     })
   )
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT!);
 }
 bootstrap();
