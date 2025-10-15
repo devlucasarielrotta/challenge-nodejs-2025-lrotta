546bd0c5e4e6f8ef823be88e3ecc31ba1d766df1
devlucasarielrotta <lucas.ariel.rotta@gmail.com>
2025-10-15 17:15:29 -0300

    Create valid routes at controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,34 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, UseFilters } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    private readonly ordersService: OrdersService
+  ) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
+  @Post('orders/:id/advance')
+  remove(@Param('id', new ParseUUIDPipe({version:'4'})) id: string) {
+    return this.ordersService.remove(id);
+  }
+  
   @Get()
   findAll() {
     return this.ordersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
+  @Get('orders/:id')
+  findOne(@Param('id', new ParseUUIDPipe({version:'4'})) id: string) {
+    return this.ordersService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(+id, updateOrderDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ordersService.remove(+id);
-  }
+ 
 }
