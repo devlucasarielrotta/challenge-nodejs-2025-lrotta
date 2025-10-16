87c8c8a7bf9cecd43cd458853108fe2977aba95f
devlucasarielrotta <lucas.ariel.rotta@gmail.com>
2025-10-16 01:04:36 -0300

    exports orders service

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
@@ -9,8 +10,12 @@ import { OrderItem } from './entities/order-item.entity';
 import { CleanupOrdersJob } from './jobs/cleanup-orders.job';
 
 @Module({
-  imports:[SequelizeModule.forFeature([Order,OrderItem])],
+  imports:[
+    SequelizeModule.forFeature([Order,OrderItem]),
+    ConfigModule
+  ],
   controllers: [OrdersController],
   providers: [OrdersService, CleanupOrdersJob],
+  exports:[OrdersService]
 })
 export class OrdersModule {}
